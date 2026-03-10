094b1cd5cab06de29f9464597ce1c444e82a4ec0
Andrey S <r014Uy78tre04@yandex.ru>
2026-03-10 17:02:51 +0500

    добавлены схемы форматов запроса и ответа к сервису погоды

---
+++ app/schemas/api_schemas.py
@@ -0,0 +1,31 @@
+from pydantic import BaseModel, field_validator
+import app.tools as tls
+
+
+class WeatherRequest(BaseModel):
+    city: str | None
+    dates: list[str]
+
+    @field_validator('city')
+    @classmethod
+    def validate_city(cls, val: str | None):
+        if not val:
+            raise ValueError(tls.lc('base.city_null'))
+
+        return val
+
+    @field_validator('dates')
+    @classmethod
+    def validate_date(cls, val: list[str]) -> list[str]:
+        for date_str in val:
+            try:
+                tls.str_to_date(date_str)
+            except ValueError:
+                raise ValueError(tls.lc('base.date_error_format'))
+
+        return val
+
+
+class WeatherResponse(BaseModel):
+    city: str
+    dates: dict[str, dict]
