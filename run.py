222cd34f84d5cb93debb630f6a80cd35faa0d7b5
Andrey S <r014Uy78tre04@yandex.ru>
2026-03-31 17:29:35 +0500

    правка запуска

--- run.py
+++ run.py
@@ -27,4 +27,4 @@ router = APIRouter()
 app.include_router(weather.router)
 
 if __name__ == "__main__":
-    uvicorn.run("run:app", reload=True)
+    uvicorn.run("run:app", host="0.0.0.0", port=8000, reload=True)
